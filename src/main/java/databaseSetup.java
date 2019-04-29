ebcd82117e6c2717cdfbba4fef8202661a477366
Greg Leonard <gr3gl@hotmail.com>
2019-04-29 18:55:15 +0100

    updates

--- src/main/java/databaseSetup.java
+++ src/main/java/databaseSetup.java
@@ -2,7 +2,7 @@ import java.sql.*;
 
 public class databaseSetup {
 
-    private static final String POSTS_TABLE = "create table posts " +
+    private static final String POSTS_TABLE = "CREATE TABLE posts " +
             " id BINARY(16) DEFAULT UUID() PRIMARY KEY, " +
             " description TEXT(63206) NOT NULL, " +
             " title VARCHAR(255), " +
@@ -16,12 +16,32 @@ public class databaseSetup {
             "   ON DELETE CASCADE " +
             "   ON UPDATE RESTRICT)";
 
-    private static final String GROUP_TABLE = "create table groups(" +
-            "id BINARY(16) DEFAULT UUID() PRIMARY KEY," +
-            "name VARCHAR(100)," +
-            "url VARCHAR(132)";
+    private static final String GROUP_TABLE = "CREATE TABLE groups(" +
+            " id BINARY(16) DEFAULT UUID() PRIMARY KEY," +
+            " name VARCHAR(100)," +
+            " url VARCHAR(132)";
 
+    private static final String UUID_TO_BIN = "CREATE FUNCTION UuidToBin(_uuid BINARY(36))" +
+            " RETURNS BINARY(16)" +
+            " LANGUAGE SQL  DETERMINISTIC  CONTAINS SQL  SQL SECURITY INVOKER " +
+            " RETURN " +
+            "   UNHEX(CONCAT( " +
+            "       SUBSTR(__uuid, 15, 4) " +
+            "       SUBSTR(__uuid, 10, 4) " +
+            "       SUBSTR(__uuid, 1, 8) " +
+            "       SUBSTR(__uuid, 20, 4) " +
+            "       SUBSTR(__uuid, 25) )) ";
 
+    private static final String BIN_TO_UUID = "CREATE FUNCTION BinToUuid(_uuid BINARY(16))" +
+            " RETURNS BINARY(36)" +
+            " LANGUAGE SQL  DETERMINISTIC  CONTAINS SQL  SQL SECURITY INVOKER " +
+            " RETURN " +
+            "   LCASE(CONACT_WS('-', " +
+            "       HEX(SUBSTR(__uuid, 5, 4)) " +
+            "       HEX(SUBSTR(__uuid, 3, 2)) " +
+            "       HEX(SUBSTR(__uuid, 1, 2)) " +
+            "       HEX(SUBSTR(__uuid, 9, 2)) " +
+            "       HEX(SUBSTR(__uuid, 11)) )) ";
 
     public static void main(String[] args){
         Statement stmt = null;
@@ -34,9 +54,9 @@ public class databaseSetup {
             connect = DriverManager.getConnection(url, username, password);
             stmt = connect.createStatement();
             stmt.executeUpdate("CREATE DATABASE data");
-            stmt.executeUpdate("USE Posts");
+            stmt.executeUpdate("USE data");
             stmt.executeUpdate(POSTS_TABLE);
-
+            stmt.executeUpdate(GROUP_TABLE);
 
         } catch (Exception e) {
             System.out.println(e);
