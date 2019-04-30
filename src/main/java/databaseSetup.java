aed9855dbfb68847d1be5b81e0c9d08cdb8d36fc
Greg Leonard <gr3gl@hotmail.com>
2019-04-30 21:30:04 +0100

    updates

--- src/main/java/databaseSetup.java
+++ src/main/java/databaseSetup.java
@@ -2,8 +2,8 @@ import java.sql.*;
 
 public class databaseSetup {
 
-    private static final String POSTS_TABLE = "CREATE TABLE posts " +
-            " id BINARY(16) DEFAULT UUID() PRIMARY KEY, " +
+    private static final String POSTS_TABLE = "CREATE TABLE posts (" +
+            " id BINARY(16) DEFAULT 0 PRIMARY KEY, " +
             " description TEXT(63206) NOT NULL, " +
             " title VARCHAR(255), " +
             " price VARCHAR(7), " +
@@ -12,36 +12,46 @@ public class databaseSetup {
             " url VARCHAR(150) NOT NULL, " +
             " group_id BINARY(16), " +
             " CONSTRAINT `fk_group_name` " +
-            "   FOREIGN KEY(group_id) REFERENCES group (id) " +
+            "   FOREIGN KEY(group_id) REFERENCES groups (id) " +
             "   ON DELETE CASCADE " +
-            "   ON UPDATE RESTRICT)";
+            "   ON UPDATE RESTRICT" +
+            " ); ";
 
-    private static final String GROUP_TABLE = "CREATE TABLE groups(" +
-            " id BINARY(16) DEFAULT UUID() PRIMARY KEY," +
+    private static final String GROUP_TABLE = "CREATE TABLE groups (" +
+            " id BINARY(16) DEFAULT 0 PRIMARY KEY," +
             " name VARCHAR(100)," +
-            " url VARCHAR(132)";
+            " url VARCHAR(132)" +
+            " );" ;
 
-    private static final String UUID_TO_BIN = "CREATE FUNCTION UuidToBin(_uuid BINARY(36))" +
+    private static final String UUID_TO_BIN = "CREATE FUNCTION UUID_TO_BIN(_uuid BINARY(36))" +
             " RETURNS BINARY(16)" +
             " LANGUAGE SQL  DETERMINISTIC  CONTAINS SQL  SQL SECURITY INVOKER " +
             " RETURN " +
             "   UNHEX(CONCAT( " +
-            "       SUBSTR(__uuid, 15, 4) " +
-            "       SUBSTR(__uuid, 10, 4) " +
-            "       SUBSTR(__uuid, 1, 8) " +
-            "       SUBSTR(__uuid, 20, 4) " +
-            "       SUBSTR(__uuid, 25) )) ";
+            "       SUBSTR(_uuid, 15, 4), " +
+            "       SUBSTR(_uuid, 10, 4), " +
+            "       SUBSTR(_uuid, 1, 8), " +
+            "       SUBSTR(_uuid, 20, 4), " +
+            "       SUBSTR(_uuid, 25) )" +
+            "   ); ";
 
-    private static final String BIN_TO_UUID = "CREATE FUNCTION BinToUuid(_uuid BINARY(16))" +
+    private static final String BIN_TO_UUID = "CREATE FUNCTION BIN_TO_UUID(_uuid BINARY(16))" +
             " RETURNS BINARY(36)" +
             " LANGUAGE SQL  DETERMINISTIC  CONTAINS SQL  SQL SECURITY INVOKER " +
             " RETURN " +
-            "   LCASE(CONACT_WS('-', " +
-            "       HEX(SUBSTR(__uuid, 5, 4)) " +
-            "       HEX(SUBSTR(__uuid, 3, 2)) " +
-            "       HEX(SUBSTR(__uuid, 1, 2)) " +
-            "       HEX(SUBSTR(__uuid, 9, 2)) " +
-            "       HEX(SUBSTR(__uuid, 11)) )) ";
+            "   LCASE(CONCAT_WS('-', " +
+            "       HEX(SUBSTR(_uuid, 5, 4)), " +
+            "       HEX(SUBSTR(_uuid, 3, 2)), " +
+            "       HEX(SUBSTR(_uuid, 1, 2)), " +
+            "       HEX(SUBSTR(_uuid, 9, 2)), " +
+            "       HEX(SUBSTR(_uuid, 11)) )) ";
+
+    private static final String UUID_TRIGGER = "CREATE TRIGGER CREATE_BIN_UUID" +
+            " BEFORE INSERT ON groups " +
+            " FOR EACH ROW " +
+            " BEGIN " +
+            "   SET NEW.id = UUID_TO_BIN(UUID());" +
+            " END; ;";
 
     public static void main(String[] args){
         Statement stmt = null;
@@ -55,8 +65,11 @@ public class databaseSetup {
             stmt = connect.createStatement();
             stmt.executeUpdate("CREATE DATABASE data");
             stmt.executeUpdate("USE data");
-            stmt.executeUpdate(POSTS_TABLE);
             stmt.executeUpdate(GROUP_TABLE);
+            stmt.executeUpdate(POSTS_TABLE);
+            stmt.executeUpdate(UUID_TO_BIN);
+            stmt.executeUpdate(BIN_TO_UUID);
+            stmt.executeUpdate(UUID_TRIGGER);
 
         } catch (Exception e) {
             System.out.println(e);
